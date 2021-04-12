a23eab62de176d30e4c1b7c5f3b99c99007d2ed0
getlike <arsentiyscherbakov@gmail.com>
2021-04-12 16:54:08 +0300

    master default add ignore view blueprint

--- app/app.py
+++ app/app.py
@@ -1,12 +1,7 @@
 from flask import Flask
+from config import Configuration
+
+
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'alloha bglsdijfuhgoiuesahrgioseughoiuregh'
-
-
-if __name__ == '__main__':
-    app.run()
+app.config.from_object(Configuration)
--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
 ##my flask app
+from app import app
+import view
+if __name__ == '__main__':
+    app.run()
